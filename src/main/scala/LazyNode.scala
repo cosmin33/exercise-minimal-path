ad0e3a4b300cb6dd0631dd9844ac359b415ab613
cosmin33 <3cosmo@gmail.com>
2024-09-05 15:57:06 +0300

    corrected the infinite positions node generation

--- src/main/scala/LazyNode.scala
+++ src/main/scala/LazyNode.scala
@@ -17,9 +17,9 @@ case class LazyLeaf[A](value: A, left: () => LazyNode[A], right: () => LazyNode[
 object LazyNode {
   
   /** returns an infinite LazyNode that contains matrix positions
-   * The first positions are (row: 1, col: 1), then the left and right jump the row and square the column
+   * The first positions are (row: 1, col: 1), then the left and right jump the row and the right jumps the column
    * so that you can get the precise positions in a (sparce) matrix with the triangle shape */
   def getInfinitePositions(row: Int, col: Int): LazyNode[(Int, Int)] =
-    LazyLeaf((row, col), () => getInfinitePositions(row + 1, col * 2 - 1), () => getInfinitePositions(row + 1, col * 2))
+    LazyLeaf((row, col), () => getInfinitePositions(row + 1, col), () => getInfinitePositions(row + 1, col + 1))
     
 }
