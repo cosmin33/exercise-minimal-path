59765483183d12bc6183334413ffb73bac1a8bde
cosmin33 <3cosmo@gmail.com>
2024-09-05 20:00:03 +0300

    removed unused code

--- src/main/scala/io/cosmo/exercise/MyApp.scala
+++ src/main/scala/io/cosmo/exercise/MyApp.scala
@@ -19,18 +19,7 @@ object MyApp extends ZIOAppDefault {
         .map(_.getMinPaths)
         .flatMap(s => printLine(s"minimal path is ${s._1.mkString(" + ")} = ${s._2}"))
     } yield ()
-
-//  val myAppLogic: ZIO[Environment & ZIOAppArgs & Scope, Any, Any] =
-//    for {
-//      fileOpt <- ZIOAppArgs.getArgs.map(_.headOption)
-//      _       <- streamToArr(readFile(fileOpt.getOrElse("./data/data.txt")))
-//        .map(SparseMatrix.apply)
-//        .map(sm => NodeLib.fromSparseMatrix(sm))
-//        .map(_.getMinPath(Position(0, 0)))
-//        .flatMap(s => printLine(s"minimal path is ${s._1.mkString(" + ")} = ${s._2}"))
-//    } yield ()
-//
-
+  
   def readFile(path: String): zio.stream.Stream[Throwable, String] =
     zio.stream.ZStream.fromFileName(path)
       .via(ZPipeline.utfDecode)
